a36518e77188a399c71b6dd65f2ad5fe77d19a4f
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-07-06 12:37:46 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Dave',
+    streetAdress: 'Cambridge BLVD',
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign({}, obj, {[key]: value});
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    return Object.assign(obj, {[key]: value});
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = Object.assign({}, obj)
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj
+}
